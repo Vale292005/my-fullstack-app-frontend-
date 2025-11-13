a48433f55ba90ad9d0089f32aeaecad410faadb2
Vale292005 <valeria.florezp@uqvirtual.edu.co>
2025-11-12 20:03:22 -0500

    card-comment

--- src/app/shared/components/molecules/card-comment/card-comment.ts
+++ src/app/shared/components/molecules/card-comment/card-comment.ts
@@ -1,18 +1,25 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+export interface Comment {
+  username: string;
+  text: string;
+  rating: number;
+}
 
 @Component({
   selector: 'app-card-comment',
   templateUrl: './card-comment.html',
   standalone: true,
+  imports: [CommonModule],
   styleUrls: ['./card-comment.css']
 })
 export class CardComment {
-  @Input() username: string = 'Usuario';
-  @Input() comment: string = 'Este es un comentario de ejemplo.';
-  @Input() rating: number = 5; // cantidad de mangos a mostrar
+  @Input() title: string = 'Sección de Comentarios';
+  @Input() comments: Comment[] = [];
 
-  getMangoArray(): number[] {
-    return Array(this.rating).fill(0);
+  getMangoArray(rating: number): number[] {
+    return Array(rating).fill(0);
   }
 }
 
